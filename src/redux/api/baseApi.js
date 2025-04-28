be6b19a6920e42a05476f236795fd436ebc082e2
Jillur <jillur@Mds-Mac-mini.local>
2025-04-28 16:57:00 +0600

    deployed url added

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://182.252.68.227:9001/api",
+    baseUrl: "https://ivs-dev.net/api",
     // baseUrl: "http://137.184.184.228/api",
     prepareHeaders: (headers, { getState }) => {
       const token = localStorage.getItem("token");
